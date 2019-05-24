1dac95db9a787058987672580b0bf533f2d1ec5a
Atif Aslam <aslam43ecehstu@gmail.com>
2019-05-25 01:44:18 +0600

    update completed

--- Django-Rest-API/Django_Rest_API/User_App/urls.py
+++ Django-Rest-API/Django_Rest_API/User_App/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 from User_App import views
 admin.autodiscover()
 urlpatterns = [
-    path('fileUpload/',views.fileUpload,name='fileUPload'),
+    path('fileUpload/',views.fileUpload),
+    path('addUser/',views.addUser),
+    path('getUser/',views.getUsers),
+    path('getUserById/<id>',views.getUserById),
+    path('deleteById/<id>',views.deleteById),
+    path('deleteAll/',views.deleteAll),
+    path('imageShow/<code>',views.imageDisplay)
 
 ]
